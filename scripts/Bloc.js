e56d1afc3c0707006a05169bee2d14556a489c62
Antoine Brassard <antoine.brassard@gmail.com>
2018-03-13 13:34:14 -0400

    fix image

--- scripts/Bloc.js
+++ scripts/Bloc.js
@@ -1,5 +1,5 @@
 class Bloc extends Case{
     constructor (intPosX, intPosY) {
-        super(intPosX, intPosY, enumTypesBlocs.objBrique.objImage);
+        super(intPosX, intPosY, enumTypesBlocs.objBloc.objImage);
     }
 }
