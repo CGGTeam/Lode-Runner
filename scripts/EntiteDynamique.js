0c4ee2ba853491e2f3719f38a7d56816d18834ba
simon <si.boyer@hotmail.ca>
2018-03-15 13:04:47 -0400

    generaliser certaines methodes de joueur -> entiteDynamique

--- scripts/EntiteDynamique.js
+++ scripts/EntiteDynamique.js
@@ -12,6 +12,12 @@ class EntiteDynamique extends Dessinable{
         this.or = 0;
         this.etatVie = true;
         this.etatAnim = AnimEnum.DEFAULT;
+        this.binBriqueGauche = false;
+        this.binBriqueDroite = false;
+        this.binBriqueHaut = false;
+        this.binBriqueBas = true;
+        this.binUp = false;
+        this.binDown = false;
 
     }
 
@@ -20,4 +26,56 @@ class EntiteDynamique extends Dessinable{
         this.intPosY = Math.round((intDeplY+this.intPosY)*10000)/10000;
     }
 
+    /**
+     * Retourne les collisions
+     * @returns {Array}
+     */
+    getCollisions() {
+        let tabObjCollisions = [];
+        let comparateur = function (e, element) {
+            if(!e || !element)
+                return true;
+            return e.intPosX === element.intPosX && e.intPosY === element.intPosY;
+        };
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.floor(this.intPosX)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.floor(this.intPosX)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
+
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY + 1)][Math.floor(this.intPosX)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY - 1)][Math.floor(this.intPosX)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY + 1)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY - 1)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
+
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.floor(this.intPosX + 1)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.floor(this.intPosX + 1)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.ceil(this.intPosX - 1)], comparateur);}catch(e){}
+        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.ceil(this.intPosX - 1)], comparateur);}catch(e){}
+
+        return tabObjCollisions;
+    }
+
+    setColBin(){
+        this.binUp = false;
+        this.binDown = false;
+        this.binBriqueGauche = false;
+        this.binBriqueDroite = false;
+        this.binBriqueHaut = false;
+        this.binBriqueBas = false;
+        this.getCollisions().forEach(value => {
+            if(value instanceof Echelle){
+                this.binUp = (this.binUp || (this.intPosX - 0.5 < value.intPosX && this.intPosX + 0.5 > value.intPosX && this.intPosY > value.intPosY - 1));
+                this.binDown = (this.binDown || (this.intPosX - 0.5 < value.intPosX && this.intPosX + 0.5 > value.intPosX && this.intPosY < value.intPosY));
+            }
+            if( value instanceof Brique){
+                this.binBriqueBas = (this.binBriqueBas || (this.intPosY + 1 === value.intPosY));
+                this.binBriqueHaut = (this.binBriqueHaut || (this.intPosY - 1 === value.intPosY));
+                this.binBriqueGauche = (this.binBriqueGauche || (this.intPosX - 1 < value.intPosX + 0.3 && this.intPosX - 1 > value.intPosX - 0.3)
+                    && this.intPosY < value.intPosY + 1 && this.intPosY > value.intPosY - 1);
+                this.binBriqueDroite = (this.binBriqueDroite || (this.intPosX + 1 < value.intPosX + 0.3 && this.intPosX + 1 > value.intPosX - 0.3)
+                    && this.intPosY < value.intPosY + 1 && this.intPosY > value.intPosY - 1);
+
+            }
+        });
+    }
 }
--- scripts/Joueur.js
+++ scripts/Joueur.js
@@ -19,12 +19,6 @@ class Joueur extends EntiteDynamique {
         this.presentKey = null;
         this.delta = 0;
         this.lastCalled = null;
-        this.binBriqueGauche = false;
-        this.binBriqueDroite = false;
-        this.binBriqueHaut = false;
-        this.binBriqueBas = true;
-        this.binUp = false;
-        this.binDown = false;
     }
 
     mettreAJourAnimation() {
@@ -104,57 +98,5 @@ class Joueur extends EntiteDynamique {
         }
     }
 
-    /**
-     * Retourne les collisions
-     * @returns {Array}
-     */
-    getCollisions() {
-        let tabObjCollisions = [];
-        let comparateur = function (e, element) {
-            if(!e || !element)
-                return true;
-            return e.intPosX === element.intPosX && e.intPosY === element.intPosY;
-        };
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.floor(this.intPosX)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.floor(this.intPosX)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
 
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY + 1)][Math.floor(this.intPosX)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY - 1)][Math.floor(this.intPosX)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY + 1)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY - 1)][Math.ceil(this.intPosX)], comparateur);}catch(e){}
-
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.floor(this.intPosX + 1)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.floor(this.intPosX + 1)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.floor(this.intPosY)][Math.ceil(this.intPosX - 1)], comparateur);}catch(e){}
-        try{tabObjCollisions.pushIfNotExist(objJeu.tabObjets[0].tabGrilleNiveau[Math.ceil(this.intPosY)][Math.ceil(this.intPosX - 1)], comparateur);}catch(e){}
-
-        return tabObjCollisions;
-    }
-
-    setColBin(){
-//Check si on peut aller vers le haut ou bas d'une echelle
-        this.binUp = false;
-        this.binDown = false;
-        this.binBriqueGauche = false;
-        this.binBriqueDroite = false;
-        this.binBriqueHaut = false;
-        this.binBriqueBas = false;
-        this.getCollisions().forEach(value => {
-            if(value instanceof Echelle){
-                this.binUp = (this.binUp || (this.intPosX - 0.5 < value.intPosX && this.intPosX + 0.5 > value.intPosX && this.intPosY > value.intPosY - 1));
-                this.binDown = (this.binDown || (this.intPosX - 0.5 < value.intPosX && this.intPosX + 0.5 > value.intPosX && this.intPosY < value.intPosY));
-            }
-            if( value instanceof Brique){
-                this.binBriqueBas = (this.binBriqueBas || (this.intPosY + 1 === value.intPosY));
-                this.binBriqueHaut = (this.binBriqueHaut || (this.intPosY - 1 === value.intPosY));
-                this.binBriqueGauche = (this.binBriqueGauche || (this.intPosX - 1 < value.intPosX + 0.3 && this.intPosX - 1 > value.intPosX - 0.3)
-                    && this.intPosY < value.intPosY + 1 && this.intPosY > value.intPosY - 1);
-                this.binBriqueDroite = (this.binBriqueDroite || (this.intPosX + 1 < value.intPosX + 0.3 && this.intPosX + 1 > value.intPosX - 0.3)
-                    && this.intPosY < value.intPosY + 1 && this.intPosY > value.intPosY - 1);
-
-            }
-        });
-    }
 }
