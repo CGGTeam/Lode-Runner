b7c41dda4f70627b7f9c8f134a183e3e232263a8
Antoine Brassard <antoine.brassard@gmail.com>
2018-03-15 20:57:54 -0400

    objNiveau accessible par objJeu

--- scripts/Jeu.js
+++ scripts/Jeu.js
@@ -3,14 +3,14 @@
 class Jeu {
     constructor() {
         //initAnimation
-        this.tabObjets = [];
-        this.tabObjets.push(new Niveau('niv1.txt'));
+        this.objNiveau;
         this.initialiserObjets();
+        this.score = 0;
         window.requestAnimationFrame(() => this.bouclePrincipale());
     }
 
     initialiserObjets() {
-
+        this.objNiveau = new Niveau('niv1.txt');
     }
 
     bouclePrincipale () {
