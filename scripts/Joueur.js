8eb7aab04eba1b70576b5a4d3f8dd45ea886d8af
Michael-John <michael.john.tech@gmail.com>
2018-03-19 18:06:42 -0400

    cleanup and removed sound to be redone.

--- scripts/Joueur.js
+++ scripts/Joueur.js
@@ -1,21 +1,21 @@
 const VITESSE_JOUEUR = 5;  // U/s
-const FPS_ANIMATION = 0.25
+const FPS_ANIMATION = 0.25;
 const KEYS_PER_SECONDS = 30;
 
 /**
  * Arrays de positions (index) dans le spritesheet, utiliser sx, sy de drawImage
  */
 const enumMapJoueur = Object.freeze({
-    RUN_R : [[0, 0],[1, 0], [2, 0]],
-    RUN_L : [[3, 0],[4, 0], [5, 0]],
-    FALL_R : [[8, 0]],
-    FALL_L : [[8, 1]],
-    CLIMB_R : [[0, 1],[1, 1], [2, 1]],
-    CLIMB_L : [[3, 1],[4, 1], [5, 1]],
-    CLIMB_U : [[6, 0],[7, 0]],
-    CLIMB_D : [[7, 0],[6, 0]],
-    DIG_R : [[6, 1]],
-    DIG_L : [[7, 1]]
+    RUN_R: [[0, 0], [1, 0], [2, 0]],
+    RUN_L: [[3, 0], [4, 0], [5, 0]],
+    FALL_R: [[8, 0]],
+    FALL_L: [[8, 1]],
+    CLIMB_R: [[0, 1], [1, 1], [2, 1]],
+    CLIMB_L: [[3, 1], [4, 1], [5, 1]],
+    CLIMB_U: [[6, 0], [7, 0]],
+    CLIMB_D: [[7, 0], [6, 0]],
+    DIG_R: [[6, 1]],
+    DIG_L: [[7, 1]]
 });
 
 class Joueur extends EntiteDynamique {
@@ -52,12 +52,12 @@ class Joueur extends EntiteDynamique {
         this.lastCalled = null;
     }
 
-    mettreAJourAnimation() {    
-        this.delta = (this.lastCalled) ? Date.now() - this.lastCalled : 1/40;
+    mettreAJourAnimation() {
+        this.delta = (this.lastCalled) ? Date.now() - this.lastCalled : 1 / 40;
         this.lastCalled = Date.now();
-        if(this.binKeyDown && !this.binFalling){
+        if (this.binKeyDown && !this.binFalling) {
             this.joueurOnKeyDown();
-        }else {
+        } else {
             //this.lastCalled = null;
         }
         if (this.objCaseCreusee) {
@@ -65,36 +65,36 @@ class Joueur extends EntiteDynamique {
         }
         this.setColBin();
         //this.binFalling = false;
-        if(!this.binBriqueBas && !this.binUp && !this.binDown){
+        if (!this.binBriqueBas && !this.binUp && !this.binDown) {
             console.log("fall");
             this.binFalling = true;
             this.binMoving = false;
-            this.deplacer(0, Math.round(VITESSE_JOUEUR * this.delta/100)/10);
-            this.tabEtatAnim = this.binMoveRight ? 
-            this.enumAnim.FALL_R : this.enumAnim.FALL_L;
+            this.deplacer(0, Math.round(VITESSE_JOUEUR * this.delta / 100) / 10);
+            this.tabEtatAnim = this.binMoveRight ?
+                this.enumAnim.FALL_R : this.enumAnim.FALL_L;
             console.log('JOUEUR X: ' + this.intPosX + ' Y: ' + this.intPosY);
         } else {
-            if(this.binFalling)
+            if (this.binFalling)
                 this.tabEtatAnim = this.enumAnim.RUN_L;
             this.binFalling = false;
-            if (this.objCaseCreusee) { 
-            this.tabEtatAnim = this.binMoveRight ? enumMapJoueur.DIG_R : enumMapJoueur.DIG_L;
-            if (this.objCaseCreusee.binDetruit) {
-                this.objCaseCreusee = null;
-                this.tabEtatAnim = this.binMoveRight ? enumMapJoueur.RUN_R : enumMapJoueur.RUN_L;
+            if (this.objCaseCreusee) {
+                this.tabEtatAnim = this.binMoveRight ? enumMapJoueur.DIG_R : enumMapJoueur.DIG_L;
+                if (this.objCaseCreusee.binDetruit) {
+                    this.objCaseCreusee = null;
+                    this.tabEtatAnim = this.binMoveRight ? enumMapJoueur.RUN_R : enumMapJoueur.RUN_L;
+                }
+            } else if (!this.binUp && !this.binDown) {
+                this.intPosY = Math.round(this.intPosY);
             }
-        } else if(!this.binUp && !this.binDown){
-            this.intPosY = Math.round(this.intPosY);
         }
-    }
-        
+
         if (this.binMoving || this.binFalling) {
             if (this.objCaseCreusee) {
                 this.objCaseCreusee.interrompreDestruction();
                 this.objCaseCreusee = null;
             }
-            this.binMoving = false;                    
-            this.dblAnimFrame += FPS_ANIMATION;            
+            this.binMoving = false;
+            this.dblAnimFrame += FPS_ANIMATION;
         }
 
         if (Math.round(this.dblAnimFrame) >= this.tabEtatAnim.length) {
@@ -107,7 +107,7 @@ class Joueur extends EntiteDynamique {
      * @param {boolean} newValue (binKeyDown)
      * @param {string} newKey (key)
      */
-    setKeyDown(newValue, newKey = null){
+    setKeyDown(newValue, newKey = null) {
         this.binKeyDown = newValue;
         this.presentKey = newKey;
     }
@@ -126,86 +126,70 @@ class Joueur extends EntiteDynamique {
         switch (this.presentKey) {
             //Left
             case 'ArrowLeft':
-                if(!this.binBriqueGauche && (this.binBriqueBas || this.binDown))
-                    this.deplacer(-VITESSE_JOUEUR * this.delta /1000, 0);
+                if (!this.binBriqueGauche && (this.binBriqueBas || this.binDown))
+                    this.deplacer(-VITESSE_JOUEUR * this.delta / 1000, 0);
                 this.tabEtatAnim = this.enumAnim.RUN_L;
-                this.binMoveRight = false;  
+                this.binMoveRight = false;
                 break;
             //Up
             case 'ArrowUp':
                 if (this.binUp) {
-                    console.log(-Math.round(VITESSE_JOUEUR * this.delta/100)/10);
+                    console.log(-Math.round(VITESSE_JOUEUR * this.delta / 100) / 10);
                     this.intPosX = Math.round(this.intPosX);
-                    this.deplacer(0, -Math.round(VITESSE_JOUEUR * this.delta/100)/10);
+                    this.deplacer(0, -Math.round(VITESSE_JOUEUR * this.delta / 100) / 10);
                     this.tabEtatAnim = this.enumAnim.CLIMB_U;
                     this.binClimb = true;
-                    this.binMoveRight = true;   
-                    instanceMoteurSon.jouerSon(0,true);
-                }else if(!this.binFalling){
-                    this.intPosY = Math.ceil(this.intPosY);               
+                    this.binMoveRight = true;
+                } else if (!this.binFalling) {
+                    this.intPosY = Math.ceil(this.intPosY);
                 }
                 break;
             //Right
             case 'ArrowRight':
-                if(!this.binBriqueDroite && (this.binBriqueBas || this.binDown))
+                if (!this.binBriqueDroite && (this.binBriqueBas || this.binDown))
                     this.deplacer(VITESSE_JOUEUR * this.delta / 1000, 0);
-                this.tabEtatAnim = this.enumAnim.RUN_R;  
-                this.binMoveRight = true;                                        
+                this.tabEtatAnim = this.enumAnim.RUN_R;
+                this.binMoveRight = true;
                 break;
             //Down
             case 'ArrowDown':
                 if (this.binDown) {
-                    this.deplacer(0, Math.round(VITESSE_JOUEUR * this.delta/100)/10);
+                    this.deplacer(0, Math.round(VITESSE_JOUEUR * this.delta / 100) / 10);
                     this.intPosX = Math.round(this.intPosX);
-                    this.tabEtatAnim = this.enumAnim.CLIMB_D;                                                              
-                    instanceMoteurSon.jouerSon(0,true);
                     this.tabEtatAnim = this.enumAnim.CLIMB_D;
-                    instanceMoteurSon.jouerSon(0,true);   
-                }else if(!this.binFalling){
-                    this.intPosY = Math.floor(this.intPosY);                                  
+                } else if (!this.binFalling) {
+                    this.intPosY = Math.floor(this.intPosY);
                 }
                 break;
             case 'x':
             case 'z':
                 console.log('dig');
-                this.binMoveRight = this.presentKey === 'x'
+                this.binMoveRight = this.presentKey === 'x';
                 let objCase = objJeu.objNiveau.tabGrilleNiveau[Math.floor(this.intPosY) + 1]
-                                                              [Math.floor(this.intPosX) - (this.presentKey === 'z' ? 1 : -1)];
+                    [Math.floor(this.intPosX) - (this.presentKey === 'z' ? 1 : -1)];
                 if (this.objCaseCreusee) {
                     this.objCaseCreusee.interrompreDestruction();
                     this.objCaseCreusee = null;
                 }
-                
+
                 if (!this.binFalling && objCase && objCase instanceof Brique) {
                     this.intPosX = Math.floor(this.intPosX);
                     this.intPosY = Math.floor(this.intPosY);
                     objCase.detruire();
                     this.dblAnimFrame = 0;
-                    this.objCaseCreusee = objCase;                    
+                    this.objCaseCreusee = objCase;
                 }
                 break;
         }
 
-        if(this.presentKey != 'ArrowUp' && this.presentKey != 'ArrowDown'){
-            instanceMoteurSon.stopperSon(0);
-        }
-
         this.getCollisions().forEach((x) => {
-            if(x instanceof Lingot){
+            if (x instanceof Lingot) {
                 instanceMoteurSon.jouerSon(4);
                 objJeu.objNiveau.tabGrilleNiveau[x.intPosY][x.intPosX] = null;
-                objJeu.objNiveau.score+=10;
+                objJeu.objNiveau.score += 10;
                 objJeu.objNiveau.updateScore();
             }
         });
-
-        if(this.getCollisions().length == 0){
-            instanceMoteurSon.jouerSon(3);
-        }
-
-        if(this.getCollisions().length != 0){
-            instanceMoteurSon.stopperSon(3);
-        }
 
     }
 }
